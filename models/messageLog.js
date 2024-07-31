16e61f5690d4b7b50dee2fe7059d0dd445061a08
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 16:31:50 +0500

    Update messageLog.js

--- models/messageLog.js
+++ models/messageLog.js
@@ -1,1 +1,15 @@
+// models/messageLog.js
+module.exports = (sequelize, DataTypes) => {
+    const MessageLog = sequelize.define('MessageLog', {
+        telegramId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        message: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    });
 
+    return MessageLog;
+};
