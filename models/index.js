b34b6c5c50c81f484c8049aba572575172e9b1c0
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 16:31:41 +0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,22 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false, // Отключить логирование SQL-запросов в консоли
+});
+
+const TelegramUser = require('./telegramUser')(sequelize, Sequelize.DataTypes);
+const MessageLog = require('./messageLog')(sequelize, Sequelize.DataTypes);
+
+sequelize.sync();
+
+module.exports = {
+    sequelize,
+    TelegramUser,
+    MessageLog,
+};
