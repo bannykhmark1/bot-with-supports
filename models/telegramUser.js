3ef23b623d617f85f77e3cb7202a213606acb47d
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-31 16:32:06 +0500

    Update telegramUser.js

--- models/telegramUser.js
+++ models/telegramUser.js
@@ -1,1 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const TelegramUser = sequelize.define('TelegramUser', {
+        telegramId: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
 
+    return TelegramUser;
+};
