624d41b66404db86c069ac512961f5a887d3e776
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-08 16:58:21 +0500

    фывфыв

--- models/image.js
+++ models/image.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
         data: {
-            type: DataTypes.BLOB('long'),
+            type: DataTypes.BYTEA, // Используем BYTEA для PostgreSQL
             allowNull: false,
         },
     });
